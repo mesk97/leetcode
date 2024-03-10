659d203b90c51d5f364d6d4022ed1f83a54feb8a
Konstantin Pakulin <mesk97@gmail.com>
2024-03-10 16:29:21 +0300

    adjust namings

--- 74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix.py
@@ -1,3 +1,38 @@
+# https://leetcode.com/problems/search-a-2d-matrix/?envType=study-plan&id=algorithm-ii
+# 74. Search a 2D Matrix
+# Medium
+# 10.9K
+# 323
+# Companies
+# You are given an m x n integer matrix matrix with the following two properties:
+
+# Each row is sorted in non-decreasing order.
+# The first integer of each row is greater than the last integer of the previous row.
+# Given an integer target, return true if target is in matrix or false otherwise.
+
+# You must write a solution in O(log(m * n)) time complexity.
+
+ 
+
+# Example 1:
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
+# Example 2:
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# Output: false
+ 
+
+# Constraints:
+
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 100
+# -104 <= matrix[i][j], target <= 104
+
 def main(matrix, target):
     last_raw = 0
     for i in range(0, len(matrix)):
